5361ce7b05bef82f33fefa7c8b9b7fa16a97b9cd
Rich Fellure <rfellure24@gmail.com>
2014-07-23 15:13:50 -0500

    MVP finished

--- MapViewController.swift
+++ MapViewController.swift
@@ -13,26 +13,10 @@ class MapViewController: UIViewController {
 
     var city = String()
 
-    @IBOutlet weak var mapView: MKMapView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let geoCoder = CLGeocoder()
-        geoCoder.geocodeAddressString(city, completionHandler: { placeMarks, error in
-            if error
-            {
-                println(error.localizedDescription)
-            }
-            else
-            {
-                let placeMark = placeMarks[0] as CLPlacemark
-
-                self.mapView.centerCoordinate = placeMark.location.coordinate
-
-                self.mapView.region.span = MKCoordinateSpanMake(1.0, 1.0)
-            }
-            })
+      navigationItem.title = city
     }
 
 
