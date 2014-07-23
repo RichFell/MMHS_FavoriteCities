062f65f3d4c50b2c4ac4f97eabaa117152962724
Rich Fellure <rfellure24@gmail.com>
2014-07-23 15:03:03 -0500

    Stretch_3 completed

---
+++ MMHS_FavoriteCities/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  MMHS_FavoriteCities
+//
+//  Created by Richard Fellure on 7/23/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+
+    @IBOutlet var tableView: UITableView!
+    var favorites = ["Columbus, OH", "Salt Lake City, UT", "Chicago, IL", "Hatteras, NC"]
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    //Action that shows our UIAlertController which we will use to add data to our Array and UITableView
+    @IBAction func onButtonPressedAddFavorite(sender: AnyObject)
+    {
+        let alert = UIAlertController(title: "Add another favorite city", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler({ textField in
+            textField.placeholder = "Add a new favorite"
+            })
+
+        let cancelAction = UIAlertAction(title: "Cancel", style:.Cancel, handler: ({ action in
+
+            }))
+        alert.addAction(cancelAction)
+
+        let saveAction = UIAlertAction(title: "Add", style: .Default, handler: ({ action in
+
+            let textField = alert.textFields[0] as UITextField
+            self.favorites.append(textField.text)
+
+            self.tableView.reloadData()
+            }))
+        alert.addAction(saveAction)
+
+        self.presentViewController(alert, animated: true, completion: nil)
+
+    }
+
+    //UITableViewDataSource function that allows us to access our UITableViewCells and change their properties
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        let city = favorites[indexPath.row] as String
+
+        cell.textLabel.text = city
+
+        return cell
+    }
+
+    //UITableViewDataSource function that allows us to determine the amount rows we will be returning
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int
+    {
+        return favorites.count
+    }
+
+    func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!)
+    {
+
+        if editingStyle == UITableViewCellEditingStyle.Delete
+        {
+            favorites.removeAtIndex(indexPath.row)
+            tableView.reloadData()
+        }
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!)
+    {
+        var mapVC = segue.destinationViewController as MapViewController
+        let indexPath = tableView.indexPathForSelectedRow()
+        mapVC.city = favorites[indexPath.row]
+        mapVC.cities = favorites
+    }
+
+
+}
+
+
---
+++ MapViewController.swift
@@ -0,0 +1,85 @@
+//
+//  MapViewController.swift
+//  MMHS_FavoriteCities
+//
+//  Created by Richard Fellure on 7/23/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, UITextFieldDelegate {
+
+    var city = String()
+    var cities = [String]()
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textField.delegate = self
+
+        moveMapCenter(city)
+
+        for favorite in cities
+        {
+            createMapAnnotations(favorite)
+        }
+    }
+
+    //Helper function that adds the annotations to the map
+    func createMapAnnotations(cityString: String)
+    {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(cityString, completionHandler: { placemarks, error in
+            if error
+            {
+                println(error.localizedDescription)
+            }
+            else
+            {
+                let placemark = placemarks[0] as CLPlacemark
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = placemark.location.coordinate
+                annotation.title = cityString
+
+                self.mapView.addAnnotation(annotation)
+            }
+            })
+    }
+
+    //UITextFieldDelegate method that is called when user presses Return key in keyboard
+    func textFieldShouldReturn(textField: UITextField!) -> Bool
+    {
+        moveMapCenter(textField.text)
+
+        return true
+    }
+
+    //Helper function for changing the center of the MKMapView
+
+    func moveMapCenter(cityString: String)
+    {
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(cityString, completionHandler: { placeMarks, error in
+            if error
+            {
+                println(error.localizedDescription)
+            }
+            else
+            {
+                let placeMark = placeMarks[0] as CLPlacemark
+
+                self.mapView.centerCoordinate = placeMark.location.coordinate
+
+                self.mapView.region.span = MKCoordinateSpanMake(1.0, 1.0)
+            }
+            })
+    }
+    
+
+
+}
